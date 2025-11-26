209ebf5b1745b24aa7046d87b96e863ab124ef06
Pratyush Kumar Nayak <111451168+prat-yussh@users.noreply.github.com>
2025-11-26 15:41:55 +0530

    Competed

---
+++ Chapter_02/Chapter2_Q1.py
@@ -0,0 +1,6 @@
+#  Write a python program to add two numbers.
+
+First_Number=int(input("Enter you first number:"))
+Second_Number=int(input("Enter you second number:"))
+
+print("Sum of Two Number is:",First_Number + Second_Number)
---
+++ Chapter_02/Chapter2_Q2.py
@@ -0,0 +1,5 @@
+# Write a python program to find remainder when a number is divided by z.
+
+a=16
+b=5
+print("Remainder when a divided by b is:", a%b)
