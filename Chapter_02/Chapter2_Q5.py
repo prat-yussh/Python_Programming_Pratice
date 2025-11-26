ac0ae44c8a79678a4a78ab79dee4d99d54fce91f
Pratyush Kumar Nayak <111451168+prat-yussh@users.noreply.github.com>
2025-11-26 15:55:19 +0530

    Completed

---
+++ Chapter_02/Chapter2_Q5.py
@@ -0,0 +1,6 @@
+# Write a python program to find an average of two numbers entered by the user.
+
+FirstNumber=int(input("Enter Your First Number:"))
+SecondNumber=int(input("Enter Your Second  Number:"))
+
+print("Avg of two number is:", (FirstNumber+SecondNumber)/2)
