c362037cb14a14f153f134c45a280f05d7e27383
Pratyush Kumar Nayak <111451168+prat-yussh@users.noreply.github.com>
2025-11-27 21:17:07 +0530

    Completed

--- Chapter_05/Chapter5_Q2.py
+++ Chapter_05/Chapter5_Q2.py
@@ -1,8 +1,22 @@
 # Write a program to input eight numbers from the user and display all the unique numbers (once).
 
-uniuqe_number=()
+uniuqe_number= set()
 
-number=input("Enter a number:")
-uniuqe_number.add()
+n=int(input("Enter a number:"))
+uniuqe_number.add(n)
+n=int(input("Enter a number:"))
+uniuqe_number.add(n)
+n=int(input("Enter a number:"))
+uniuqe_number.add(n)
+n=int(input("Enter a number:"))
+uniuqe_number.add(n)
+n=int(input("Enter a number:"))
+uniuqe_number.add(n)
+n=int(input("Enter a number:"))
+uniuqe_number.add(n)
+n=int(input("Enter a number:"))
+uniuqe_number.add(n)
+n=int(input("Enter a number:"))
+uniuqe_number.add(n)
 
 print(uniuqe_number)
