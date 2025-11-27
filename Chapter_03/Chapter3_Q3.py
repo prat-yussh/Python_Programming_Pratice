26508761a56687bf84f1768728994ce92026b070
Pratyush Kumar Nayak <111451168+prat-yussh@users.noreply.github.com>
2025-11-27 14:07:40 +0530

    Completed

--- Chapter_03/Chapter3_Q3.py
+++ Chapter_03/Chapter3_Q3.py
@@ -1,3 +1,5 @@
+# Write a program to detect double space in a string. 
+
 str="hello my name  is pratyush."
 
 print(str.find("  "))
---
+++ Chapter_03/Chapter3_Q4.py
@@ -0,0 +1,9 @@
+'''
+3. Write a program to detect double space in a string. 
+4. Replace the double space from problem 3 with single spaces.
+'''
+
+str="hello my name  is pratyush."
+
+# print(str.find("  "))
+print(str.replace("  "," "))
