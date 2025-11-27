b07c6c81a37742a8acadf7db0f176d4d3b36c6ec
Pratyush Kumar Nayak <111451168+prat-yussh@users.noreply.github.com>
2025-11-27 15:00:52 +0530

    Completed

--- Chapter_04/Chapter4_Q3.py
+++ Chapter_04/Chapter4_Q3.py
@@ -1,1 +1,5 @@
 # Check that a tuple type cannot be changed in python. 
+
+var=("omm",100,50,"pratyush")
+
+var[0]="hello"
--- Chapter_04/Chapter4_Q4.py
+++ Chapter_04/Chapter4_Q4.py
@@ -1,1 +1,5 @@
 # Write a program to sum a list with 4 numbers.
+
+var=[10,10,10,10]
+
+print(sum(var))
