b07fe9c7a16509ceb4301778d4a1e0d9b32bdea8
Pratyush Kumar Nayak <111451168+prat-yussh@users.noreply.github.com>
2025-11-27 19:33:25 +0530

    Completed

---
+++ Chapter_05/Chapter5_Q1.py
@@ -0,0 +1,11 @@
+# Write a program to create a dictionary of Hindi words with values as their English translation. Provide user with an option to look it up!
+
+hindi = {
+    "anda": "egg",
+    "khao": "eat",
+    "daudo": "run"
+}
+
+word=input("Enter the word you want to know")
+
+print(hindi[word])
