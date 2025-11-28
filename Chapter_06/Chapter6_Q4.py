e2c59257486b779bde6ee59cf0bcbdc924fd328f
Pratyush Kumar Nayak <111451168+prat-yussh@users.noreply.github.com>
2025-11-28 15:42:57 +0530

    Completed

---
+++ Chapter_06/Chapter6_Q4.py
@@ -0,0 +1,13 @@
+# Write a program to find whether a given username contains less than 10 characters or not. 
+
+# username=input("Enter your username:")
+
+# number_of_character=username.count()
+
+# if(number_of_character>10):
+#     print("thid username is valid")
+# else:
+#     print("thid username is not valid")
+
+s="hello"
+print(count(s))
