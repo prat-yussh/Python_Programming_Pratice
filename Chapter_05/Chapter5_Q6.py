5a8ea0db2836d632857330e487b8c210f264131b
Pratyush Kumar Nayak <111451168+prat-yussh@users.noreply.github.com>
2025-11-28 08:43:51 +0530

    Completed

---
+++ Chapter_05/Chapter5_Q6.py
@@ -0,0 +1,9 @@
+#  Create an empty dictionary. Allow 4 friends to enter their favorite language as value and use key as their names. Assume that the names are unique. 
+
+database={}
+
+data=input("enter your fav subject")
+database=data
+
+print(database)
+print(type(database))
