224c669f563269c5b1ccb283e988117a9e15be25
Pratyush Kumar Nayak <111451168+prat-yussh@users.noreply.github.com>
2025-11-29 10:37:53 +0530

    Completed

--- Chapter_06/Chapter6_Q5.py
+++ Chapter_06/Chapter6_Q5.py
@@ -1,12 +1,10 @@
 #  Write a program which finds out whether a given name is present in a list or not. 
 
-names=["pratyush","omm"]
+names=["pratyush","omm","kakasi","naruto","harry"]
 
 user=input("Enter the name you want to find:")
 
-if(user==names[0] or user==names[1]):
+if(user in names):
     print("The name is present in the list")
 else:
     print("The name is not present in the list")
-
-# print(names[0])
