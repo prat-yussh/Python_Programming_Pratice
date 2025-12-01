f4002237786a75c6c04cac56d3ec9d0be010db4d
Pratyush Kumar Nayak <111451168+prat-yussh@users.noreply.github.com>
2025-12-01 12:07:21 +0530

    Completed

--- Chapter_07/Chapter7_Q7.py
+++ Chapter_07/Chapter7_Q7.py
@@ -7,7 +7,7 @@ Write a program to print the following star pattern.
 
 n=int(input("Enter a number:"))
 
-for i in range(1, n+1):
+for i in range(1,n+1):
     print(" "*(n-i),end="")
     print("*"*(2*i-1),end="")
-    print(" ")
+    print("")
