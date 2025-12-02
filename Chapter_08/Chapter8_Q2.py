4dd06c42daf2c8da78560e16111efdf70cb9ab4b
Pratyush Kumar Nayak <111451168+prat-yussh@users.noreply.github.com>
2025-12-02 12:10:53 +0530

    Completed

--- Chapter_08/Chapter8_Q2.py
+++ Chapter_08/Chapter8_Q2.py
@@ -1,7 +1,7 @@
 # Write a python program using function to convert Celsius to Fahrenheit. 
 
 def converter(c):
-    ferhanite=(c*9/5)+35
+    ferhanite=(c*9/5)+32
     print(ferhanite)
 
 converter(18)
