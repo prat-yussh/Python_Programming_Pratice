d4db074030e4c58d2f1f506f018f44266ae1d896
Pratyush Kumar Nayak <111451168+prat-yussh@users.noreply.github.com>
2025-12-03 22:56:52 +0530

    Completed

--- Chapter_08/Chapter8_Q5.py
+++ Chapter_08/Chapter8_Q5.py
@@ -8,7 +8,7 @@ Write a python function to print first n lines of the following pattern:
 def display(n):
     for i in range(1,n+1):
         print("*",end="")
-        print("*",end="")
+        print("*",end="") 
 n=int(input("Enter a number"))
 display(n)
 
