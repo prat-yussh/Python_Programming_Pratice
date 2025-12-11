19fc3a2a4d10ad811ed44c53e532c69cfd447c8a
Pratyush Kumar Nayak <111451168+prat-yussh@users.noreply.github.com>
2025-12-11 22:06:25 +0530

    Completed

--- Chapter_09/Chapter9_Q3.py
+++ Chapter_09/Chapter9_Q3.py
@@ -1,2 +1,8 @@
 # Write a program to generate multiplication tables from 2 to 20 and write it to the different files. Place these files in a folder for a 13 – year old. 
-
+with open("Multi_Plication_Table.txt", "w+") as f:
+    for i in range(2,21):
+        print("Best Of Luck Buddy😊")
+        print("")
+        print(f"This is Multiplication Table of:{i}")
+        for j in range(1,11):
+            print(f"{i} X {j} = {i*j}")
