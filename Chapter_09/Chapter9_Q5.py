2dd1108fa958df36ebc8aea78b44f10faa727f69
Pratyush Kumar Nayak <111451168+prat-yussh@users.noreply.github.com>
2025-12-31 15:29:09 +0530

    Completed

--- Chapter_09/Chapter9_Q5.py
+++ Chapter_09/Chapter9_Q5.py
@@ -2,3 +2,9 @@
 4. A file contains a word “Donkey” multiple times. You need to write a program which replace this word with ##### by updating the same file.  
 5. Repeat program 4 for a list of such words to be censored. 
 """
+
+with open("Chapter9_Q4(Donkey).txt", "r") as f:
+    content=f.read().strip().lower()
+    updated_content=content.replace("donkey","#####")
+    with open("Chapter9_Q4(Donkey).txt", "w") as f:
+        f.write(updated_content)
