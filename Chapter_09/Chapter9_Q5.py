feb70e42a1749dee75c20b4ae9ae2eea0c0b0df2
Pratyush Kumar Nayak <111451168+prat-yussh@users.noreply.github.com>
2025-12-31 15:28:44 +0530

    Completed

---
+++ Chapter_09/Chapter9_Q5.py
@@ -0,0 +1,4 @@
+"""
+4. A file contains a word “Donkey” multiple times. You need to write a program which replace this word with ##### by updating the same file.  
+5. Repeat program 4 for a list of such words to be censored. 
+"""
