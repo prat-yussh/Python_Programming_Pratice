4b8a6abb49aaa397edd35d86b5dba4f41d288ddb
Pratyush Kumar Nayak <111451168+prat-yussh@users.noreply.github.com>
2026-01-01 15:19:18 +0530

    Completed

--- Chapter_10/Chapter10_Q2.py
+++ Chapter_10/Chapter10_Q2.py
@@ -1,1 +1,19 @@
 # Write a class “Calculator” capable of finding square, cube and square root of a number. 
+
+import math
+
+class Calculator:
+    def __init__(self, square, cube, sqrt):
+        self.square=square*square
+        self.cube=cube ** 3
+        self.sqrt=math.sqrt(sqrt)
+    
+    def __str__(self):
+        return(
+            f"square={self.square}\n"
+            f"cube={self.cube}\n"
+            f"sqrt={self.sqrt}"
+        )
+
+v=Calculator(4, 4, 4)
+print(v)
