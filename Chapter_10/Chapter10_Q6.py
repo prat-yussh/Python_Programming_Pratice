82e78eff1aec1c8ac2bf4e036864532d38202136
Pratyush Kumar Nayak <111451168+prat-yussh@users.noreply.github.com>
2026-01-02 15:24:14 +0530

    Completed

--- Chapter_10/Chapter10_Q6.py
+++ Chapter_10/Chapter10_Q6.py
@@ -1,1 +1,22 @@
 #  Can you change the self-parameter inside a class to something else (say “harry”). Try changing self to “slf” or “harry” and see the effects.
+
+from random import randint
+
+class Train:
+
+    def __init__(slf, trainNo): #there is no problem 
+        slf.trainNo=trainNo
+
+    def book(harry, fro, to):
+        print(f"The train is booked in train no {harry.trainNo} from {fro} to {to}") #there is no problem 
+
+    def getStatus(self, fro, to):
+        print(f"The train is going from {fro} to {to} and number of seats available are {randint(100, 500)}")
+
+    def getFare(self, fro, to):
+        print(f"The fair of the train is {randint(100, 500)} which is going from {fro} to {to}")
+
+obj=Train(123)
+obj.book("delhi", "mumbai")
+obj.getStatus("delhi", "mumbai")
+obj.getFare("delhi", "mumbai")
