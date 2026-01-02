669f4e8edb656cf4a7c732fd133ba3e3cfceb851
Pratyush Kumar Nayak <111451168+prat-yussh@users.noreply.github.com>
2026-01-02 11:40:47 +0530

    Completed

--- Chapter_10/Chapter10_Q3.py
+++ Chapter_10/Chapter10_Q3.py
@@ -1,1 +1,9 @@
 #  Create a class with a class attribute a; create an object from it and set ‘a’ directly using ‘object.a = 0’. Does this change the class attribute? 
+
+class demo:
+    a=5
+
+obj=demo() #obj allocation
+obj.a=0 #set value
+print(obj.a) #print new seted value
+print(demo.a) #but here the class attribute is not changed
