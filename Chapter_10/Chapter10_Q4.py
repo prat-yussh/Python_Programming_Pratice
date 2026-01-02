a63b8864e16f43ab07b9681f746c83cc86f735e1
Pratyush Kumar Nayak <111451168+prat-yussh@users.noreply.github.com>
2026-01-02 11:56:09 +0530

    Completed

--- Chapter_10/Chapter10_Q4.py
+++ Chapter_10/Chapter10_Q4.py
@@ -1,1 +1,25 @@
 # Add a static method in problem 2, to greet the user with hello. 
+
+# Write a class “Calculator” capable of finding square, cube and square root of a number. 
+import math
+
+class Calculator:
+    def __init__(self, square, cube, sqrt):
+        self.square=square*square
+        self.cube=cube ** 3
+        self.sqrt=math.sqrt(sqrt)
+    
+    def __str__(self):
+        return(
+            f"square={self.square}\n"
+            f"cube={self.cube}\n"
+            f"sqrt={self.sqrt}"
+        )
+    
+    @staticmethod
+    def demo():
+        print("Here is your answer.")
+
+v=Calculator(4, 4, 4)
+print(v)
+v.demo()
