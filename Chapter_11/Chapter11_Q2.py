b58db298b4fc8e0b30a1e7dc6a69ee5aa1a0c7e6
Pratyush Kumar Nayak <111451168+prat-yussh@users.noreply.github.com>
2026-01-04 15:07:31 +0530

    Completed

--- Chapter_11/Chapter11_Q2.py
+++ Chapter_11/Chapter11_Q2.py
@@ -1,1 +1,20 @@
 # Create a class ‘Pets’ from a class ‘Animals’ and further create a class ‘Dog’ from ‘Pets’. Add a method ‘bark’ to class ‘Dog’.
+
+class Animals:
+    def __init__(self):
+        pass
+
+class Pets(Animals):
+    def __init__(self):
+        super().__init__()
+
+class Dog(Pets):
+    def __init__(self):
+        super().__init__()
+
+    
+    def bark(self):
+        print("Woof Woof!")
+
+obj=Dog()
+obj.bark()
