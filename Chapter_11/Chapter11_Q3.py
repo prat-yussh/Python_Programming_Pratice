c8baf89b1ecc947f320559d451dae6e538350d99
Pratyush Kumar Nayak <111451168+prat-yussh@users.noreply.github.com>
2026-01-04 16:26:24 +0530

    Completed

--- Chapter_11/Chapter11_Q3.py
+++ Chapter_11/Chapter11_Q3.py
@@ -5,3 +5,12 @@ Write a method ‘salaryAfterIncrement’ method with a @property decorator with
 
 which changes the value of increment based on the salary.
 """
+
+class Employee:
+    def __init__(self, name, salery, saleryIncrement):
+        self.name=name
+        self.salery=salery
+        self.saleryIncrement=self.salery+500
+
+o=Employee("Pratyush",500)
+
