6e425c1970bc691a7f5083a69b0b86710523581d
Pratyush Kumar Nayak <111451168+prat-yussh@users.noreply.github.com>
2026-01-04 14:49:15 +0530

    Completed

--- Chapter_11/Chapter11_Q1.py
+++ Chapter_11/Chapter11_Q1.py
@@ -1,18 +1,24 @@
 # Create a class (2-D vector) and use it to create another class representing a 3-D vector.
 
 class twoDvector:
-    def var(self, x, y):
-        self.x=x
-        self.y=y
-        print(f"These are the value of {self.x}x and {self.y}y ")
+
+    def __init__(self, i, j):
+        self.i=i
+        self.j=j
+
+    def var(self):
+        print(f"These are the value of {self.i}i and {self.j}j ")
 
 class threeDvector(twoDvector):
-    def var(self,x, y, z):
-        super().var(x,y)
-        self.z=z
-        print(f"These are the value of {self.x}x,{self.y}y and {self.z}z")
 
-o=twoDvector()
-o.var(1,2)
-o2=threeDvector()
-o2.var(3,4, 5)
+    def __init__(self, i, j, k):
+        super().__init__(i,j)
+        self.k=k
+
+    def var(self):
+        print(f"These are the value of {self.i}i , {self.j}j and {self.k}k")
+
+o=twoDvector(1, 2)
+o.var()
+o2=threeDvector(1, 2, 3)
+o2.var()
