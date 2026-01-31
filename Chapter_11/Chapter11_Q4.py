e284b5aacc7fd544405404cee2fd047e39386153
Pratyush Kumar Nayak <111451168+prat-yussh@users.noreply.github.com>
2026-01-31 14:04:13 +0530

    not completed

--- Chapter_11/Chapter11_Q4.py
+++ Chapter_11/Chapter11_Q4.py
@@ -1,1 +1,28 @@
 # Write a class ‘Complex’ to represent complex numbers, along with overloaded operators ‘+’ and ‘*’ which adds and multiplies them. 
+
+class Complex:
+    def __init__(self, real, imag):
+        self.real = real
+        self.imag = imag
+
+    def __add__(self, other):
+        return Complex(
+            self.real + other.real,
+            self.imag + other.imag
+        )
+
+    def __mul__(self, other):
+        return Complex(
+            self.real * other.real - self.imag * other.imag,
+            self.real * other.imag + self.imag * other.real
+        )
+
+    def __str__(self):
+        sign = "+" if self.imag >= 0 else "-"
+        return f"{self.real} {sign} {abs(self.imag)}i"
+
+c1 = Complex(3, 2)
+c2 = Complex(1, 7)
+
+print("Addition:", c1 + c2)
+print("Multiplication:", c1 * c2)
