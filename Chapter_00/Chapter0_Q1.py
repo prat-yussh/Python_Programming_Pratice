791175553e564d3d114662f3cdf13de2e69c1791
Pratyush Kumar Nayak <111451168+prat-yussh@users.noreply.github.com>
2026-02-08 18:22:19 +0530

    Completed

---
+++ Chapter_00/Chapter0_Q1.py
@@ -0,0 +1,16 @@
+'''
+Which of the following are valid Python identifiers? Explain why.
+
+a) total
+b) 2total
+c) _value
+d) for
+e) total_marks
+
+'''
+
+total=0 #this is valid varriable because it starts with a word
+#2total=0 #this is not a valid varriable because it starts with a number
+_value=0 #this is valid varriable because it starts with a underscore
+#for=0 #this is not a valid varriable because it starts with a reserved word
+total_marks=0 #this is valid varriable because it starts with a word
