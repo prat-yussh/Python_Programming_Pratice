4b97a88b5914cb85601efe8ab5afb30f1b8553f7
Pratyush Kumar Nayak <111451168+prat-yussh@users.noreply.github.com>
2026-02-12 14:50:47 +0530

    Completed

---
+++ Chapter_06/Chapter6_Q16.py
@@ -0,0 +1,11 @@
+"""
+What is wrong here?
+
+x = 1
+while x < 5:
+    print(x)
+"""
+x = 1
+while x < 5:
+    print(x)
+#x is not incremnted so it is infinate loop
---
+++ Chapter_06/Chapter6_Q17.py
@@ -0,0 +1,17 @@
+"""
+for i in range(1, 6):
+    if i % 2 == 0:
+        continue
+    print(i, end=" ")
+
+Output?
+"""
+
+for i in range(1, 6):
+    if i % 2 == 0:
+        continue
+    print(i, end=" ")
+"""
+Output:-
+1 3 5
+"""
