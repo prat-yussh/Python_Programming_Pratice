27d54ab3fee49c39a79787db96aea92abf000bcd
Pratyush Kumar Nayak <111451168+prat-yussh@users.noreply.github.com>
2026-02-12 14:59:18 +0530

    Completed

--- Chapter_06/Chapter6_Q18.py
+++ Chapter_06/Chapter6_Q18.py
@@ -1,1 +1,27 @@
-print(2 % 4 == 0)
+"""
+cart = [10, 20, 600, 30]
+for item in cart:
+    if item > 500:
+        print("Stop")
+        break
+    print(item)
+else:
+    print("All processed")
+
+    
+Will "All processed" print?
+"""
+
+cart = [10, 20, 600, 30]
+for item in cart:
+    if item > 500:
+        print("Stop")
+        break
+    print(item)
+else:
+    print("All processed")
+# "All processed" will not print because once loop hit item > 500 it come out of the loop
+# output
+# 10 
+# 20
+# Stop
---
+++ Chapter_06/Chapter6_Q19.py
@@ -0,0 +1,15 @@
+"""
+Pattern Logic
+If n = 3, what is output?
+
+for i in range(1, n+1):
+    print("*" * i)
+"""
+n=3
+for i in range(1, n+1):
+    print("*" * i)
+"""
+*
+**
+***
+"""
