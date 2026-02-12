a0049e8a75d2c616d759c2ea107829b73efbed06
Pratyush Kumar Nayak <111451168+prat-yussh@users.noreply.github.com>
2026-02-12 14:43:23 +0530

    Completed

---
+++ Chapter_06/Chapter6_Q15.py
@@ -0,0 +1,14 @@
+"""
+Continue trap
+for i in range(5):
+    if i == 2:
+        continue
+    print(i)
+
+What prints?
+"""
+for i in range(5):
+    if i == 2:
+        continue
+    print(i)
+# the output will be 0 1 3 4
