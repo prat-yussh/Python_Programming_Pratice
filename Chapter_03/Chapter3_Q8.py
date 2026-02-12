97a726c6da1ab579e9cd823325477725f1805b04
Pratyush Kumar Nayak <111451168+prat-yussh@users.noreply.github.com>
2026-02-12 13:53:07 +0530

    Completed

---
+++ Chapter_03/Chapter3_Q8.py
@@ -0,0 +1,9 @@
+"""
+What is the output?
+print("Hello" + "World")
+print("Hello", "World")
+
+Explain difference in one line.
+"""
+print("Hello" + "World") #HelloWorld "+" is concatinate two str
+print("Hello", "World") #Hello World "," separate two str and add a space between them
