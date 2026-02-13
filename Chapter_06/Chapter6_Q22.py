a42b61c04465850d8ead3217ec33da098c419cf5
Pratyush Kumar Nayak <111451168+prat-yussh@users.noreply.github.com>
2026-02-13 11:00:21 +0530

    Completed

--- Chapter_06/Chapter6_Q22.py
+++ Chapter_06/Chapter6_Q22.py
@@ -15,3 +15,11 @@ for i in range(1,num):
         sum=sum+i
     
 print("Sum of all even numbers from 1 to n:{}".format(sum))
+"""
+here are the logic steps
+
+step-1:user takes input and create a "sum" varriable
+step-2:A loop starts from 1 to user entered "num"
+step-3:If condition starts and checks the number is even or not
+step-4:if condition satisfied the add to "sum" varriable
+"""
---
+++ Chapter_06/Chapter6_Q23.py
@@ -0,0 +1,23 @@
+"""
+User enters a number.
+
+Count how many digits it has.
+
+(No using len(str(n)) — think mathematically.)
+"""
+num=int(input("Enter a number:"))
+
+if num<0:
+    num= -num
+
+if num==0:
+    count=1
+    # print("Number of digits are:{}".format(count))
+    
+else:
+    count=0
+    while num>0:
+        num = num//10
+        count=count+1
+
+print("Number of digits are:{}".format(count))
