228b63a89104fcb52fe7fe6233d3c94833644ca2
Pratyush Kumar Nayak <111451168+prat-yussh@users.noreply.github.com>
2026-02-13 10:21:46 +0530

    Completed

---
+++ Chapter_06/Chapter6_Q20.py
@@ -0,0 +1,25 @@
+"""
+User enters a number.
+
+Print:
+"Divisible by 3"
+"Divisible by 5"
+"Divisible by both"
+"Not divisible by either"
+
+How will you structure conditions?
+(Think about order carefully.)
+"""
+num=int(input("Enter a number:"))
+
+if num%3==0 and num%5==0:
+    print("the number is divisble by 3.")
+
+elif num%3!=0 and num%5!=0:
+    print("the number is divisble by 5.")
+
+elif num%3==0:
+    print("the number is divisble by both 3 and 5.")
+
+elif num%5==0:
+    print("the number is divisble by 5.")
