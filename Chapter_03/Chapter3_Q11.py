d5f0ebd7d429c61ecc57363cedf0113cd1269e12
Pratyush Kumar Nayak <111451168+prat-yussh@users.noreply.github.com>
2026-03-24 19:15:59 +0530

    Completed

--- Chapter_03/Chapter3_Q11.py
+++ Chapter_03/Chapter3_Q11.py
@@ -1,3 +1,18 @@
 """
+Character Access
+User enters a string.
 
+Print:
+First character
+Last character
+
+Example
+Input: Python
+Output:
+First: P
+Last: n
 """
+
+str=input("Enter a string find out first and last charcter:")
+
+print("First:{} \nLast:{}".format(str[0],str[-1]))
