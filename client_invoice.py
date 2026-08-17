169e6643fab1a46e58882a41cdd4c1e1de698cff
Pratyush Kumar Nayak <111451168+prat-yussh@users.noreply.github.com>
2026-08-17 20:23:23 +0530

    Completed

--- client_invoice.py
+++ client_invoice.py
@@ -1,28 +1,60 @@
-# Dictionary Q6 — get() + update() + condition
+# Dictionary Q7 — Nested Dictionary + Loop
 
-# You have:
+# Now let's do one important nested-dictionary question and then move toward finishing dictionaries.
 
-# Do all of this:
+# Do all of these:
 
-# Use get() to retrieve "name".
-# Use get() to retrieve "email" with "Not available" as the default value.
+# Print Riya's salary.
+# Loop through the outer dictionary using items().
 
-# Use update() to add:
+# For each employee, print:
 
-# "email": "asha@company.com"
-# "experience": 2
-# Increase the salary to 50000.
-# Print the final dictionary.
+# EMP101 : Asha : 45000
+# Count how many employees are in the "IT" department.
+# Find the highest salary manually.
 
-# No loop needed.
+# Print:
 
-employee = {
-    "name": "Asha",
-    "department": "IT",
-    "salary": 45000
+# IT employees:
+# Highest salary:
+# Rules
+# Use one loop.
+# Use items().
+# Do not use max().
+# Don't create another loop.
+
+employees = {
+    "EMP101": {
+        "name": "Asha",
+        "department": "IT",
+        "salary": 45000
+    },
+    "EMP102": {
+        "name": "Rahul",
+        "department": "HR",
+        "salary": 38000
+    },
+    "EMP103": {
+        "name": "Riya",
+        "department": "IT",
+        "salary": 52000
+    }
 }
 
-employee.update({"email": "asha@company.com","experience": 2,"salary": 50000})
-print(employee.get(["name"]))
-print(employee.get("email","Not available"))
-print(employee)
+it_dept=0
+highest_salary = 0
+highest_salary_emp_id = 0
+
+
+# print(employees)
+for employe_id,details in employees.items():
+    print(employe_id,details["name"],details["salary"],sep=" : ")
+    if details["department"] == "IT":
+        it_dept += 1
+        print("IT employees:",details["name"])
+    if details["salary"] > highest_salary:
+        highest_salary_emp_id = employe_id
+        highest_salary = details["salary"]
+        
+print("Highest salary:",highest_salary)
+    
